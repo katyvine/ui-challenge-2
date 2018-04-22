a672a59fa1e9631360feaf4c6c9776f8c2630c95
Katy Bowers <katyvine@gmail.com>
2018-04-22 12:01:05 -0700

    buttons toggle once

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ $(function()  {
     $('button.dropdown').on('click', function(e){
     e.preventDefault();
     $('ul').toggle();
-    console.log("live dropdown button");
-    $("button.dropdown").css("background-position","-640px -160px");
+    $("button.dropdown").css("background-position", "-640px -160px");
+  });
+
+  $('button.checkbox').on('click', function(e){
+    e.preventDefault();
+  $('button.checkbox').css("background-position", "-220px -130px");
+  });
+
+  $('button.radio').on('click', function(e){
+    e.preventDefault();
+  $('button.radio').css("background-position", "-500px -130px");
   });
 });
